82e75c3869d5b003a4ad4c429be01174160134f8
pyr <jzhangnv@gmail.com>
2026-01-07 21:12:53 -0500

    Added way to fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import './styles.css'
+// Weather App:
+// Key: 264GN7TF2UVPDJRRGJRZL2GLP
+// 1. Grab user input
+//      - Use input to fetch data
+//      ** URL ENCODED
+//      - 
+// 2. Format Data
+//      - Use ol & li to list out data 
+//          - Date
+//          - Temp (hi & lo)
+//          - Wind speed
+//          - Sunrise & set
+//          - Description
 
-document.body.textContent = "deadbeef";
+import './styles.css'
+class Weather{
+    #key;
+    constructor(loc){
+        this.loc = loc;
+        this.url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+        this.#key = '264GN7TF2UVPDJRRGJRZL2GLP';
+    }
+    getWeather(){
+        console.log(this.#getWeatherData());
+    }
+    async #getWeatherData() {
+        const response = await fetch(`${this.url}${this.loc}?key=${this.#key}`).then((response) => {return response.json()});
+        return response;
+    }
+}
+
+let las_vegas = new Weather("las vegas");
+las_vegas.getWeather();
