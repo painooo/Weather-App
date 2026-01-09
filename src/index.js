0183097d8d78f0f8dc5c60ee8169516d0c4305eb
pyr <jzhangnv@gmail.com>
2026-01-08 20:17:06 -0500

    Removed useless comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 // Weather App:
 // Key: 264GN7TF2UVPDJRRGJRZL2GLP
-// 1. Grab user input
-//      - Use input to fetch data
-//      ** URL ENCODED
-//      - 
-// 2. Format Data
-//      - (days)
-//      - Use ol & li to list out data 
-//          - Date
-//          - Temp (hi & lo)
-//          - Wind speed
-//          - Sunrise & set
-//          - Description
-
 import './styles.css'
 import Weather from './Weather.js'
 import WeatherDOM from './WeatherDOM.js'
